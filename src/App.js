9aec37b3d094df963df5dc6effdabbfc3baeb95d
Knigh11 <tuedang039@gmail.com>
2023-10-26 18:55:23 +0700

    Add new project food

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+
+function Apppicture(props) {
+  console.log(props);
+  return (
+    <div className="picture">
+      <img src={props.image} alt=""></img>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Apppicture image="https://c4.wallpaperflare.com/wallpaper/39/346/426/digital-art-men-city-futuristic-night-hd-wallpaper-preview.jpg"></Apppicture>
   );
 }
 
